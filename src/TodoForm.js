103f4c45a89589fdaad04b65bc6f471d6154d5c0
junior-8080 <aabdulmukhsin@yahoo.com>
2021-06-30 15:32:59 +0000

    small change

--- src/TodoForm.js
+++ src/TodoForm.js
@@ -36,7 +36,7 @@ class  TodoForm extends Component{
     render(){
         return(
             <form onSubmit={this.handleSubmit}>
-                <h1 className="title">MY CURRENT TODO LIST </h1>
+                <h2 className="title">MY TODO LIST </h2>
                 <div className="todo-form">
                     <input type="text" value={this.state.text} onChange={this.handleChange} placeholder="Enter your task"/>
                     <button type="button"  className="clear" onClick={this.handleSubmit}>Add</button>
